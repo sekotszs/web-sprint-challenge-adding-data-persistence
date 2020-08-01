c2903038539ae8f6654b416bd8993b5d6bbb276b
Zoe Stokes <sekotszs@gmail.com>
2020-07-31 18:58:15 -0700

    tables are made. need to add seeds

--- data/migrations/20200731183113_tables.js
+++ data/migrations/20200731183113_tables.js
@@ -1,8 +1,34 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("project", (table) => {
+    table.increments("id");
+    table.text("name").notNull();
+    table.text("description").notNull();
+    table.integer("complete",false);
+  });
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("resource", (table) => {
+    table.increments("id");
+    table.text("name").notNull();
+    table.text("description");
+  });
+
+  await knex.schema.createTable("task", (table) => {
+    table.increments("id");
+    table.text("description").notNull();
+    table.text("notes").notNull();
+    table.integer("complete",false);
+  });
+
+  await knex.schema.createTable("task_resource", (table) => {
+      table.increments("id")
+    table.integer("task_id").references("id").inTable("task");
+    table.integer("resource_id").references("id").inTable("resource");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("project");
+  await knex.schema.dropTableIfExists("resource");
+  await knex.schema.dropTableIfExists("task");
+  await knex.schema.dropTableIfExists("task_resource");
 };
